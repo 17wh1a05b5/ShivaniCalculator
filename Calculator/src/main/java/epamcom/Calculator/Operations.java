e9bb88739d552fe4cac3abb0b86672ceed46fd30
17wh1a05b5 <17wh1a05b5@bvrithyderabad.edu.in>
2020-03-03 09:13:58 +0530

    Created a New Maven Project

---
+++ Calculator/src/main/java/epamcom/Calculator/Operations.java
@@ -0,0 +1,17 @@
+package epamcom.Calculator;
+
+public class Operations {
+	double add(double x, double y) {
+		return x + y;
+	}
+	double substract(double x, double y) {
+		return x - y;
+	}
+	double multiply(double x, double y) {
+		return x * y;
+	}
+	double divide(double x, double y) {
+		return x / y;
+	}
+
+}
